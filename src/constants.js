69da4a0acd04beb9f5c1e715b719c68d5117c292
To Hai Nam <namthit98@MacBook-Pro.local>
2020-05-12 02:46:54 +0700

    add env

--- src/constants.js
+++ src/constants.js
@@ -6,6 +6,6 @@ export const ROLES = {
 };
 
 export const CORE = {
-  SERVER_URL: process.env.SERVER_URL || 'http://localhost:7000/',
+  SERVER_URL: process.env.REACT_SERVER_URL || 'http://localhost:7000/',
   S3_URL: process.env.S3_URL || 'https://codingame.s3-ap-southeast-1.amazonaws.com'
 }
