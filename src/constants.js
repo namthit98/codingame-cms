978d97a18243f27a18ab433fff3029143de5ac97
To Hai Nam <namthit98@MacBook-Pro.local>
2020-05-12 14:04:17 +0700

    update

--- src/constants.js
+++ src/constants.js
@@ -6,6 +6,8 @@ export const ROLES = {
 };
 
 export const CORE = {
-  SERVER_URL: "https://quiet-waters-32044.herokuapp.com/" || 'http://localhost:7000/',
-  S3_URL: process.env.S3_URL || 'https://codingame.s3-ap-southeast-1.amazonaws.com'
-}
+  SERVER_URL:
+    "https://quiet-waters-32044.herokuapp.com/" || "http://localhost:7000/",
+  S3_URL:
+    process.env.S3_URL || "https://codingame.s3-ap-southeast-1.amazonaws.com",
+};
